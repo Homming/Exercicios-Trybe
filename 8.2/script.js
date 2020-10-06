6aed0ee592c7c6f35ef91dd7ff42316a15169279
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2020-10-06 19:28:37 -0300

    Função de retorno com generos de livros fantasia e ficção criado

--- 8.2/script.js
+++ 8.2/script.js
@@ -99,8 +99,51 @@ const expected_result2 = [
     }
   ];
 
+const expected_result3 = [
+    {
+       id: 1,
+       name: 'As Crônicas de Gelo e Fogo',
+       genre: 'Fantasia',
+       author: {
+         name: 'George R. R. Martin',
+         birthYear: 1948
+       },
+       releaseYear: 1991,
+     },
+     {
+       id: 2,
+       name: 'O Senhor dos Anéis',
+       genre: 'Fantasia',
+       author: {
+         name: 'J. R. R. Tolkien',
+         birthYear: 1892,
+       },
+       releaseYear: 1954,
+     },
+     {
+       id: 3,
+       name: 'Fundação',
+       genre: 'Ficção Científica',
+       author: {
+         name: 'Isaac Asimov',
+         birthYear: 1920,
+       },
+       releaseYear: 1951,
+     },
+     {
+       id: 4,
+       name: 'Duna',
+       genre: 'Ficção Científica',
+       author: {
+         name: 'Frank Herbert',
+         birthYear: 1920,
+       },
+       releaseYear: 1965,
+     },
+   ]
+
 function formatedBookNames() {
-  return books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
+    return books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
 }
 
 function nameAndAge() {
@@ -114,8 +157,12 @@ function nameAndAge() {
     return authorAgeRelease.sort((a, b) => {
         return a.age - b.age;
     });
-  }
+}
   
+function fantasyOrScienceFiction() {
+    return books.filter(book => book.genre === "Fantasia" || book.genre === "Ficção Científica");
+}
 
+assert.deepEqual(fantasyOrScienceFiction(), expected_result3);
 assert.deepEqual(nameAndAge(), expected_result2);
 assert.deepEqual(formatedBookNames(), expected_result1);
